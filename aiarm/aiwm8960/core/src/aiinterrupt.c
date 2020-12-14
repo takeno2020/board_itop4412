f54aae3199d63b1c9acf9a6ad2a32b3d7059479c
takeno2020 <takeno2020@163.com>
2020-12-14 13:03:29 +0800

    声卡wm8960裸机程序:基本程序框架和编译、链接脚本的实现.

---
+++ aiarm/aiwm8960/core/src/aiinterrupt.c
@@ -0,0 +1,22 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  aiinterrupt.c
+ *
+ *    Description:  中断处理函数
+ *
+ *        Version:  1.0
+ *        Created:  12/14/2020 12:15:23 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  takeno (takeno2020), takeno2020@163.com
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+void do_irq()
+{
+
+    /* empty */
+}
